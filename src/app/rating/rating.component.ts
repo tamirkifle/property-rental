18b5812909216b48f30fb0642624ed9d4e8deea9
Tamir K. Yirga <tkifle@gmail.com>
2020-09-04 16:49:24 +0300

    refactored generateRating with helper fns

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -24,78 +24,42 @@ export class RatingComponent implements OnInit {
     }
   }
   generateRating() {
-    if (this.rating > 0 && this.rating < 1) {
-      this.stars[0].classList.add('fa-star-half-o');
-    } else if (this.rating === 1) {
-      this.stars[0].classList.remove('fa-star-o');
-      // this.stars[0].classList.add('fa-star');
-    } else if (this.rating > 1 && this.rating < 2) {
-      this.stars[0].classList.remove('fa-star-o');
-      this.stars[1].classList.add('fa-star-half-o');
-    } else if (this.rating === 2) {
-      this.stars[0].classList.remove('fa-star-o');
-      this.stars[1].classList.remove('fa-star-o');
-    } else if (this.rating > 2 && this.rating < 3) {
-      this.stars[0].classList.remove('fa-star-o');
-      this.stars[1].classList.remove('fa-star-o');
-      this.stars[2].classList.add('fa-star-half-o');
-    } else if (this.rating === 3) {
-      this.stars[0].classList.remove('fa-star-o');
-      this.stars[1].classList.remove('fa-star-o');
-      this.stars[2].classList.remove('fa-star-o');
-    } else if (this.rating > 3 && this.rating < 4) {
-      this.stars[0].classList.remove('fa-star-o');
-      this.stars[1].classList.remove('fa-star-o');
-      this.stars[2].classList.remove('fa-star-o');
-      this.stars[3].classList.add('fa-star-half-o');
-    } else if (this.rating === 4) {
-      this.stars[0].classList.remove('fa-star-o');
-      this.stars[1].classList.remove('fa-star-o');
-      this.stars[2].classList.remove('fa-star-o');
-      this.stars[3].classList.remove('fa-star-o');
-    } else if (this.rating > 4 && this.rating < 5) {
-      this.stars[0].classList.remove('fa-star-o');
-      this.stars[1].classList.remove('fa-star-o');
-      this.stars[2].classList.remove('fa-star-o');
-      this.stars[3].classList.remove('fa-star-o');
-      this.stars[4].classList.add('fa-star-half-o');
-    } else if (this.rating === 5) {
-      this.stars[0].classList.remove('fa-star-o');
-      this.stars[1].classList.remove('fa-star-o');
-      this.stars[2].classList.remove('fa-star-o');
-      this.stars[3].classList.remove('fa-star-o');
-      this.stars[4].classList.remove('fa-star-o');
+    this.fillStarsUntil(this.rating - 1, '#28b6f6');
+    if (this.rating - Math.floor(this.rating) > 0){
+      this.stars[Math.floor(this.rating)].classList.add('fa-star-half-o');
+      this.stars[Math.floor(this.rating)].style.color = '#28b6f6';
     }
   }
-
+  private removeFillAllStars() {
+    this.stars.forEach((star: HTMLElement) => {
+      star.classList.add('fa-star-o');
+      star.style.color = '#707070';
+    });
+  }
+  private fillStarsUntil(index = this.stars.length, color?) {
+    this.removeFillAllStars();
+    this.stars.some((star, i) => {
+      if (i > index) {
+        return true;
+      }
+      star.classList.remove('fa-star-o');
+      if(color){
+        star.style.color = color;
+      }
+    });
+  }
   generateInputRating() {
     function isFilled(star) {
       return !star.classList.contains('fa-star-o');
     }
-    let removeFillAllStars = () => {
-      this.stars.forEach((star: HTMLElement) => {
-        star.classList.add('fa-star-o');
-        star.style.color = '#707070';
 
-      });
-    };
-    let fillStarsUntil = (index = this.stars.length) => {
-      removeFillAllStars();
-      this.stars.some((star, i) => {
-        if(i > index){
-          return true;
-        }
-        star.classList.remove('fa-star-o');
-        star.style.color = '#28b667';
-
-      });
-    };
     let getStarsInfo = (clickedStar) => {
       let clickedIndex = -1;
       let filledAfterClickedStar = false;
       this.stars.some((star, i) => {
         if (clickedIndex !== -1) {
-          if (isFilled(star)) {      // Are there stars filled after the clicked star?
+          if (isFilled(star)) {
+            // Are there stars filled after the clicked star?
             filledAfterClickedStar = true;
             return true;
           }
@@ -104,20 +68,20 @@ export class RatingComponent implements OnInit {
           clickedIndex = i;
         }
       });
-      return {clickedIndex, filledAfterClickedStar};
+      return { clickedIndex, filledAfterClickedStar };
     };
 
     let fillStars = (e): void => {
-      let {clickedIndex, filledAfterClickedStar} = getStarsInfo(e.currentTarget);
-      if (filledAfterClickedStar){
-          fillStarsUntil(clickedIndex);
-      }
-      else{
-        if(isFilled(e.currentTarget)){
-          removeFillAllStars();
-        }
-        else{
-          fillStarsUntil(clickedIndex);
+      let { clickedIndex, filledAfterClickedStar } = getStarsInfo(
+        e.currentTarget
+      );
+      if (filledAfterClickedStar) {
+        this.fillStarsUntil(clickedIndex, '#28b667');
+      } else {
+        if (isFilled(e.currentTarget)) {
+          this.removeFillAllStars();
+        } else {
+          this.fillStarsUntil(clickedIndex, '#28b667');
         }
       }
     };
