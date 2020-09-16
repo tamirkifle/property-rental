fdf74a3b9ed2ab24a7f94ee9be3ad2962fc75ad9
Tamir K. Yirga <tkifle@gmail.com>
2020-09-16 21:47:26 +0300

    added create account page

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateUserComponent } from './create-user/create-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: CreateUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,11 +5,13 @@ import { UserRoutingModule } from './user-routing.module';
 import { UserLineComponent } from './user-line/user-line.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { SharedModule } from '../shared/shared.module';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 @NgModule({
   declarations: [
     UserLineComponent,
     UserDetailComponent,
+    CreateUserComponent,
   ],
   imports: [
     CommonModule,
