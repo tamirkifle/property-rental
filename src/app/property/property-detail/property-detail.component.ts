aa49f200040a18b4533c43839d8b61961870a147
Tamir K. Yirga <tkifle@gmail.com>
2020-11-07 13:49:35 +0300

    show edit btn if authorized to edit only

--- src/app/property/property-detail/property-detail.component.ts
+++ src/app/property/property-detail/property-detail.component.ts
@@ -17,6 +17,7 @@ import { MediaChange, MediaObserver } from '@angular/flex-layout';
 export class PropertyDetailComponent implements OnInit {
   property: Property;
   postCreatorUser: User;
+  isAuthorizedToEdit = false;
   imageObject: Array<object>;
   media$: Observable<MediaChange[]>;
   customImageSize = { width: '100%', height: '500px', space: 0 };
@@ -48,6 +49,7 @@ export class PropertyDetailComponent implements OnInit {
     });
     this.property = this.route.snapshot.data.property;
     this.postCreatorUser = this.route.snapshot.data.user;
+    this.isAuthorizedToEdit = this.authService.isLoggedIn && (this.authService.currentUser.username === this.postCreatorUser.username);
     this.imageObject = this.property.propertyImages.map(imageLink => {
       return { image: imageLink, thumbImage: imageLink, title: this.property.propertyTitle };
     });
@@ -55,10 +57,5 @@ export class PropertyDetailComponent implements OnInit {
 
   goBack(): void {
     this.router.navigate(['../..'], { relativeTo: this.route, queryParamsHandling: 'preserve' });
-  }
-
-  get isAuthorizedToEdit() {
-    console.log(this.authService.isLoggedIn, this.authService.currentUser);
-    return this.authService.isLoggedIn && (this.authService.currentUser.username === this.postCreatorUser.username);
   }
 }
