81fe838c7d94cb610d5480ac5c14dbb7a777198a
Tamir K. Yirga <tkifle@gmail.com>
2020-11-09 02:50:25 +0300

    fixed not redirecting to login page problem

--- src/app/auth/role.guard.ts
+++ src/app/auth/role.guard.ts
@@ -1,28 +1,22 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    return this.authService.authState$.pipe(
-      map(user => {
-        if (user) {
-          return true;
-        }
-        else {
-          this.authService.redirectUrl = state.url.split('?')[0];
-          this.router.navigate(['/login']);
-          return false;
-        }
-      }));
+    if (this.authService.isLoggedIn){
+      return true;
     }
+    this.authService.redirectUrl = state.url.split('?')[0];
+    this.router.navigate(['/login']);
+    return false;
+  }
 }
